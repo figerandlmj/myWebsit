e7ddd16d7c1413f283b74406e81bdbf9e83c4625
limanjiao <limanjiao@letto8.com>
2018-11-02 18:32:45 +0800

    first commit

---
+++ src/h5/loanMarket/loanMarket.js
@@ -0,0 +1,10 @@
+import Vue from 'vue'
+import App from './App'
+import 'assets/css/reset'
+import 'assets/js/buryingPoint'
+
+new Vue({
+  el: '#app',
+  template: '<App/>',
+  components: { App }
+})
